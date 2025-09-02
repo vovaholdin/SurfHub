064b74aff194408f2b839a504982a96c5a4532dc
Vova Goldin <mrlolka1000@gmail.com>
2025-09-02 14:14:08 +0100

    ADD liquibase master, init, and data changelogs

--- src/main/java/com/goldin/config/DataBaseConfig.java
+++ src/main/java/com/goldin/config/DataBaseConfig.java
@@ -4,13 +4,16 @@ import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class DataBaseConfig {
 
     @Bean
-    public SpringLiquibase liquibase() {
+    public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setChangeLog("classpath:db/changelog.xml");
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/master.yaml");
         return liquibase;
     }
 }
