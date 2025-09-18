0487d1719d8da619dd3fc4d54743cadf10aec687
Vova Goldin <mrlolka1000@gmail.com>
2025-09-18 17:49:11 +0300

    fix timeZone

--- src/main/java/com/goldin/config/WebConfig.java
+++ src/main/java/com/goldin/config/WebConfig.java
@@ -10,10 +10,15 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.TimeZone;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.goldin")
 public class WebConfig implements WebApplicationInitializer {
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
+    }
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
